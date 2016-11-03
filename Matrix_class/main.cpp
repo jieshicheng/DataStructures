90ac9afcd9b9ed3e780429eb4692f37d50c67451
jieshicheng <951497836@qq.com>
2016-11-03 17:20:24 +0800

    Matrix by array class

--- Matrix_class/main.cpp
+++ Matrix_class/main.cpp
@@ -1,3 +1,15 @@
+/************************************
+
+
+@file    matrix class
+@author  jieshicheng
+@data    2 Nov 2016
+@remark  matrix by unary array
+@note    this is a test txt
+
+
+************************************/
+
 #include"matrix.hpp"
 
 #include<iostream>
