0ce6c3ce182d6a4634979c890d75315709ed1ae6
jieshicheng <951497836@qq.com>
2016-11-06 09:34:23 +0800

    hash list by chainlist

--- HashChains_class/main.cpp
+++ HashChains_class/main.cpp
@@ -1,3 +1,13 @@
+/***********************************
+
+@file    hashChain
+@author  jieshicheng
+@data    6 Nov 2016
+@remark  hashList by chainList
+@note    this is test txt
+
+************************************/
+
 #include"hashChain.hpp"
 
 #include<iostream>
