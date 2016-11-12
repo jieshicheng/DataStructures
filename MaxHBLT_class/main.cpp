c63c0f944dbe2452c078ee03c70e8b95856b96eb
jieshicheng <951497836@qq.com>
2016-11-12 16:44:54 +0800

    max HBLT by chain

--- MaxHBLT_class/main.cpp
+++ MaxHBLT_class/main.cpp
@@ -1,4 +1,15 @@
+/**********************************************
+
+@file      max height biased leftist tree
+@author    jieshicheng
+@data      9 Nov 2016
+@remark    max HBLT by chain
+@note      this is a test txt
+
+**********************************************/
+
 #include"maxHblt.hpp"
+
 #include<iostream>
 
 using namespace std;
